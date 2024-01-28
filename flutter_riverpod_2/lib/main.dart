df7d84ab92541886d5f84d3ce7be438a609686c9
JarlissonTomaz <tomazj995@gmail.com>
2024-01-28 16:33:38 -0300

    riverpod 2

--- flutter_riverpod_2/lib/main.dart
+++ flutter_riverpod_2/lib/main.dart
@@ -5,25 +5,64 @@ import 'package:flutter_riverpod_2/post.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final postsFutureProvider = FutureProvider<List<Post>>((ref) async {
-  try {
-    await Future.delayed(const Duration(seconds: 2));
-    final response =
-        await http.get(Uri.https('jsonplaceholder.typicode.com', 'posts'));
-    if (response.statusCode == 200) {
-      final body = json.decode(response.body) as List;
-      final List<Post> posts = body.map((dynamic json) {
-        return Post(
-            id: json['id'] as int,
-            title: json['title'] as String,
-            body: json['body'] as String);
-      }).toList();
-      return posts;
-    } else {
-      throw Exception('Erro ao buscar postagens');
+class PostsRepository {
+  Future<List<Post>> fetchPostsLocal() async {
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      final response =
+          await http.get(Uri.https('jsonplaceholder.typicode.com', 'posts'));
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body) as List;
+        final List<Post> posts = body.map((dynamic json) {
+          return Post(
+              id: json['id'] as int,
+              title: json['title'] as String,
+              body: json['body'] as String);
+        }).toList();
+        return posts;
+      } else {
+        throw Exception('Erro ao buscar postagens');
+      }
+    } on Exception {
+      throw Exception();
     }
-  } on Exception {
-    throw Exception();
+  }
+
+  Future<List<Post>> fetchPostsRemote() async {
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      final response =
+          await http.get(Uri.https('jsonplaceholder.typicode.com', 'posts'));
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body) as List;
+        final List<Post> posts = body.map((dynamic json) {
+          return Post(
+              id: json['id'] as int,
+              title: json['title'] as String,
+              body: json['body'] as String);
+        }).toList();
+        return posts;
+      } else {
+        throw Exception('Erro ao buscar postagens');
+      }
+    } on Exception {
+      throw Exception();
+    }
+  }
+}
+
+final postsRepositoryProvider = Provider(
+  (ref) => PostsRepository(),
+);
+
+final postsFutureProvider =
+    FutureProvider.family<List<Post>, String>((ref, typeDb) async {
+  final postsRepository = ref.watch(postsRepositoryProvider);
+
+  if (typeDb == 'local') {
+    return postsRepository.fetchPostsLocal();
+  } else {
+    return postsRepository.fetchPostsRemote();
   }
 });
 
@@ -52,9 +91,23 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final posts = ref.watch(postsFutureProvider);
+    AsyncValue<List<Post>> posts = ref.watch(postsFutureProvider('local'));
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: const Text('Flutter Riverpod 2'),
+      ),
+      body: posts.when(
+        data: (data) => ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(data[index].title),
+            );
+          },
+        ),
+        error: (e, st) => const Text('Erro ao buscar postagens'),
+        loading: () => const Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 }
